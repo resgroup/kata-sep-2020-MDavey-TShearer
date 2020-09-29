99347216ff0b785079ddc6535aae8c70b83ffc93
Thomas Shearer <thomas.shearer@res-group.com>
2020-09-29 12:01:00 +0100

    Retrospective 3 / third iteration

--- main.py
+++ main.py
@@ -3,52 +3,61 @@ from enum import Enum
 class TennisGame:    
     def __init__(self, server_score, receiver_score):
         self.game_over = False
+        self.winner = ''
         self.score = TennisScore(server_score,receiver_score)
 
     def server_wins_point(self):
-        self.score.increment_server_score()
-        if (self.score.server.score.WIN):
-            self.game_over = True
-            self.winner = 'server'
+        if self.game_over == False:
+            self.score.increment_server_score()
+            if (self.score.server_score == Scores.WIN):
+                self.game_over = True
+                self.winner = 'server'
         
-    def reciever_wins_point(self):
-        self.score.increment_receiver_score()
-        if (self.score.receiver.score.WIN):
-            self.game_over = True
-            self.winner = 'receiver'
+    def receiver_wins_point(self):
+        if self.game_over == False:
+            self.score.increment_receiver_score()
+            if (self.score.receiver_score == Scores.WIN):
+                self.game_over = True
+                self.winner = 'receiver'
+    
+    def get_score_string(self):
+        if self.game_over == True:
+            score_string = '{} Wins'.format(self.winner)
+        elif self.score.server_score == self.score.receiver_score == Scores.FORTY:
+            score_string = 'DEUCE'
+        else:
+            score_string_dict = {'LOVE':'0','FIFTEEN':'15','THIRTY':'30','FORTY':'40','ADVANTAGE':'A'}
+            score_string = f'{score_string_dict[self.score.server_score.name]}:{score_string_dict[self.score.receiver_score.name]}'
+        return score_string
     
         
 class TennisScore:
     def __init__(self, server_score, receiver_score):
-        self.server = TennisPlayer('server', server_score)
-        self.receiver = TennisPlayer('receiver', receiver_score)
-
-    def increment_score(self, score, player):
-        if player.score == Scores.ADVANTAGE:
-            player.score = Scores.WIN
-        
-        else: 
-            score = Scores(score.value + 1)
-        if score == [Scores.FORTY,Scores.FORTY]:
-            score = player.score = DEUCE
-        return score
+        self.server_score = server_score
+        self.receiver_score = receiver_score
 
     def increment_server_score(self):
-        self.server.score = self.increment_score(self.receiver.score)
-
+        self.server_score = Scores(self.server_score.value + 1)
+        self.validate_scores()
+    
     def increment_receiver_score(self):
-        self.receiver.score = self.increment_score(self.server.score)
+        self.receiver_score = Scores(self.receiver_score.value + 1)
+        self.validate_scores()
 
-class TennisPlayer:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
+    def validate_scores(self):
+        if (self.server_score == Scores.ADVANTAGE and self.receiver_score.value < Scores.FORTY.value):
+            self.server_score = Scores.WIN
+        if (self.receiver_score == Scores.ADVANTAGE and self.receiver_score.value < Scores.FORTY.value):
+            self.receiver_score = Scores.WIN
+        elif (self.server_score == self.receiver_score == Scores.ADVANTAGE):
+            self.server_score = Scores.FORTY
+            self.receiver_score = Scores.FORTY
 
 class Scores(Enum):
     LOVE = 0
     FIFTEEN = 1
     THIRTY = 2
     FORTY = 3
-    DEUCE = 4
-    ADVANTAGE = 5 
-    WIN = 6
+    ADVANTAGE = 4
+    WIN = 5
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,27 +3,27 @@ from main import TennisGame, Scores
 def test_server_wins_first_point():
     game = TennisGame(Scores.LOVE,Scores.LOVE)
     game.server_wins_point()
-    assert game.score == [Scores.FIFTEEN,Scores.LOVE]
+    assert game.get_score_string() == "15:0"
 
 def test_receiver_wins_point_15_15():
     game = TennisGame(Scores.FIFTEEN,Scores.FIFTEEN)
     game.receiver_wins_point()
-    assert game.score == [Scores.FIFTEEN,Scores.THIRTY] 
+    assert game.get_score_string() == "15:30"
     
 def test_server_wins_point_30_30():
     game = TennisGame(Scores.THIRTY,Scores.THIRTY)
     game.server_wins_point()
-    assert game.score == [Scores.FORTY,Scores.THIRTY]
+    assert game.get_score_string() == "40:30"
 
 def test_deuce():
     game = TennisGame(Scores.FORTY, Scores.FORTY)
     game.receiver_wins_point()
-    assert game.score == [Scores.FORTY,Scores.ADVANTAGE]
+    assert game.get_score_string() == "40:A"
 
 def test_advantage():
     game = TennisGame(Scores.ADVANTAGE, Scores.FORTY)
     game.receiver_wins_point()
-    assert game.score == [Scores.FORTY, Scores.FORTY]
+    assert game.get_score_string() == "DEUCE"
 
 def test_winning_game():
     game = TennisGame(Scores.FORTY, Scores.THIRTY)
