7a575d04e27ed06cadb9a5ece02614039a5ceb2a
Thomas Shearer <thomas.shearer@res-group.com>
2020-09-29 09:48:12 +0100

    First iteration
    Co-authored-by: matthew.davey@res-group.com <matthew.davey@res-group.com>

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def function1(x, y):
-    return x + y
+def create_game(server_score, receiver_score):
+    return TennisGame(server_score, receiver_score)
 
-def function2(x, y):
-    return x - y
+
+class TennisGame:    
+    def __init__(self, server_score, receiver_score):
+        self.game_over = False
+        self.server_score = server_score
+        self.receiver_score = receiver_score
+        self.scores = [0,15,30,40]
+        self.score = 
+
+    def server_wins_point(self):
+        if score = [0,0]
+        #self.scores.index(self.server_score)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,52 @@
-from main import function1, function2
+from main import create_game
 
-def test_function1():
-    assert function1(1, 1) == 2
+def test_winning_game():
+    game = create_game(40, 30)
+    game.server_wins_point()
+    assert (game.game_over == True and game.winner == 'server')
 
-def test_function2():
-    assert function2(1, 1) == 0
+def test_winning_game_from_advantage():
+    game = create_game(40, "A")
+    game.receiver_wins_point()
+    assert (game.game_over == True and game.winner == 'receiver')
+
+def test_deuce():
+    game = create_game(40, 40)
+    game.receiver_wins_point()
+    assert game.score == [40,"A"]
+
+def test_advantage():
+    game = create_game("A", 40)
+    game.receiver_wins_point()
+    assert game.score == [40, 40]
+
+def test_server_wins_first_point():
+    game = create_game(0,0)
+    game.server_wins_point()
+    assert game.score == [15,0]
+
+def test_receiver_wins_point_15_15():
+    game = create_game(15,15)
+    game.receiver_wins_point()
+    assert game.score == [15,30] 
+    
+def test_server_wins_point_30_30():
+    game = create_game(30,30)
+    game.server_wins_point()
+    assert game.score == [40,30] 
+    
+def test_full_game_with_deuce():
+    game = create_game(0,0)
+    game.server_wins_point()
+    game.server_wins_point()
+    game.receiver_wins_point()
+    game.server_wins_point()
+    game.receiver_wins_point()
+    game.receiver_wins_point()
+    game.receiver_wins_point()
+    game.server_wins_point()
+    game.receiver_wins_point()
+    game.receiver_wins_point()
+    assert (game.game_over == True and game.winner == 'receiver')
+
+     
